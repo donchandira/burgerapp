52d2ec301c2b461eea6d82d9852d26dc752ad9ba
Don Withanage <don@armaturesystems.com>
2019-11-24 14:21:57 -0800

    Added order button disable/enable

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,8 +19,19 @@ class BurgerBuilder extends Component{
       cheese: 0,
       meat: 0
     },
-    totalPrice: 4
+    totalPrice: 4,
+    puchasable: false
 
+  }
+
+  updatePurchasableState (ingredients) {
+
+    const sum = Object.keys(ingredients).map(igKey => {
+      return ingredients[igKey]
+    }).reduce((sum, el) => {
+      return sum + el;
+    }, 0);
+    this.setState({purchasable: sum > 0});
   }
 
   addIngredientHandler = (type) => {
@@ -34,6 +45,7 @@ class BurgerBuilder extends Component{
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice + priceAddition;
     this.setState({totalPrice: newPrice, ingredients: updateIngredients })
+    this.updatePurchasableState(updateIngredients);
   }
 
   removeIngredientHandler = (type) => {
@@ -51,6 +63,7 @@ class BurgerBuilder extends Component{
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice - priceDeduction;
     this.setState({totalPrice: newPrice, ingredients: updateIngredients })
+    this.updatePurchasableState(updateIngredients);
   }
 
   render(){
@@ -69,6 +82,7 @@ class BurgerBuilder extends Component{
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
+          purchasable={this.state.purchasable}
           price={this.state.totalPrice}
         />
       </Aux>
